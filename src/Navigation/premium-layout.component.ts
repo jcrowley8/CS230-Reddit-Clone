b80b79211d7fec52c5f1f00da311ebf02ad6e41f
Jared <jc0223@mix.wvu.edu>
2022-02-20 21:49:18 -0500

    added components to premium

--- src/Navigation/premium-layout.component.ts
+++ src/Navigation/premium-layout.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'reddit-premium-layout',
     templateUrl: 'premium-layout.component.html',
+    styleUrls: ['premium-layout.component.css'],
 })
 
 
