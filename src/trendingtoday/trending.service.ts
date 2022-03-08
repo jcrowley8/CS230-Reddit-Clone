ec421802df2fbd795f9970d811b6cc667cb95968
Jared <jc0223@mix.wvu.edu>
2022-03-08 18:43:26 -0500

    format changes

--- src/trendingtoday/trending.service.ts
+++ src/trendingtoday/trending.service.ts
@@ -6,18 +6,15 @@ import { TrendingTodayModel } from "./trendingtoday_model";
 @Injectable({
     providedIn: 'root'
 })
-export class TrendingService{
+export class TrendingService {
     private baseUrl: string = "https://reddit-6f039-default-rtdb.firebaseio.com/";
     private trendingEndpoint: string = "trendingToday.json";
-
-    constructor(private http:HttpClient){
+    constructor(private http: HttpClient) {
     }
-    public getTrending(){
-        return this.http.get<TrendingTodayModel []>(this.baseUrl + this.trendingEndpoint);
+    public getTrending() {
+        return this.http.get<TrendingTodayModel[]>(this.baseUrl + this.trendingEndpoint);
     }
-
-    public getTrendings(index:number){
+    public getTrendings(index: number) {
         return this.http.get<TrendingTodayModel>(this.baseUrl + 'trendingToday/' + index + '.json');
     }
-
 }
--- src/trendingtoday/trendinglayout.component.ts
+++ src/trendingtoday/trendinglayout.component.ts
@@ -4,24 +4,19 @@ import { TrendingService } from "./trending.service";
 import { TrendingTodayModel } from "./trendingtoday_model";
 
 @Component({
-    selector: 'reddit-trendingtodaylayout',
-    templateUrl: 'trendinglayout.component.html',
-    styleUrls: ['trendingtoday.component.css']
+  selector: 'reddit-trendingtodaylayout',
+  templateUrl: 'trendinglayout.component.html',
+  styleUrls: ['trendingtoday.component.css']
 })
 
-
-
-export class TrendingTodayLayoutComponent implements OnInit{
-    trendingtoday: TrendingTodayModel [] = [];
-
-    constructor(private trendingservice: TrendingService){
-      
-    }
-
+export class TrendingTodayLayoutComponent implements OnInit {
+  trendingtoday: TrendingTodayModel[] = [];
+  constructor(private trendingservice: TrendingService) {
+  }
   ngOnInit(): void {
     this.trendingservice.getTrending().subscribe(data => {
       console.log("Fetching trending today data");
-      for(var trending of data){
+      for (var trending of data) {
         console.log(trending);
         this.trendingtoday.push(trending);
       }
